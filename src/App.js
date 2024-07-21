065cfd4331ea75b2785e06853da058e4c9c00be9
agta <angeloabrita@gmail.com>
2024-07-21 01:02:18 -0300

    :broom: cleanup: limpeza de codigo e nao acessiveis

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import 'tuicss';
-
-
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
--- src/components/Links.js
+++ src/components/Links.js
@@ -5,7 +5,7 @@ const BtnLinks = () =>{
 
     return(
         <div className="center">
-                <Link to="/" className="tui-button white-168" >Resumo</Link>
+                <Link to="/" className="tui-button yellow-168" >Resumo</Link>
                 <Link to="/experiencia" className="tui-button white-168" >Experiencia</Link>
                 <Link to="/educacao" className="tui-button green-168">Educação</Link>
                 <Link to="/certificacoes" className="tui-button red-168">Certifiações</Link>
