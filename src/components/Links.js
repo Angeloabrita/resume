857f28d6fa4ffe4c9170952c6214375099537690
agta <angeloabrita@gmail.com>
2024-07-21 22:14:01 -0300

    :recycle: refactor: Correção ortografica

--- src/components/Links.js
+++ src/components/Links.js
@@ -8,7 +8,7 @@ const BtnLinks = () =>{
                 <Link to="/" className="tui-button yellow-168" >Resumo</Link>
                 <Link to="/experiencia" className="tui-button white-168" >Experiencia</Link>
                 <Link to="/educacao" className="tui-button green-168">Educação</Link>
-                <Link to="/certificacoes" className="tui-button red-168">Certifiações</Link>
+                <Link to="/certificacoes" className="tui-button red-168">Certificações</Link>
                 <Link to="/premiacoes" className="tui-button blue-168">Premiações</Link>
                 <Link to="/idiomas" className="tui-button purple-168">Idiomas</Link>
                 <Link to="/curriculum" className="tui-button cyan-168">Curriculo completo</Link>
